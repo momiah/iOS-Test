355501cf613ba442e849d04a865c1254f053d400
mdmiah <mdmiah@gmail.com>
2015-09-09 13:24:17 +0100

    Controls are not hidden when keyboard is displayed

--- Tests/Autolayout/Autolayout/Autolayout/ViewController.swift
+++ Tests/Autolayout/Autolayout/Autolayout/ViewController.swift
@@ -12,12 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name:UIKeyboardWillShowNotification, object: nil);
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name:UIKeyboardWillHideNotification, object: nil);
+
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func keyboardWillShow(sender: NSNotification) {
+        self.view.frame = CGRectOffset(self.view.superview!.frame, 0, -100);
+    }
+    
+    func keyboardWillHide(sender: NSNotification) {
+        self.view.frame = CGRectOffset(self.view.superview!.frame, 0, 0);
     }
 
 
